46c815f4de4012e52883a0ca5c57a048743b41b3
Paulo Magalhaes <paulomagalhaes@Paulos-MacBook.objectivespo>
2015-02-19 19:54:35 -0200

    publishing to s3

--- project/Build.scala
+++ project/Build.scala
@@ -122,8 +122,8 @@ object IScalaBuild extends Build {
                 val github = url("https://raw.githubusercontent.com/mattpap/mvn-repo/master/releases")
                 Resolver.url("github-releases", github)(Resolver.ivyStylePatterns)
             },
-          resolvers += "Uberdata Maven Repo" at "s3://uberdata-repo",
-          publishTo := Some("Uberdata Maven Repo" at "s3://uberdata-repo/snapshots"),
+          resolvers += "Uberdata Maven Repo" at "s3://uberdata-repo/releases/",
+          publishTo := Some("Uberdata Maven Repo" at "s3://uberdata-repo/snapshots/"),
           libraryDependencies ++= {
                 import Dependencies._
                 scalaio ++ Seq(ivy.value, scopt, jeromq, play_json, slick, sqlite,  specs2, compiler.value, spark)
